ada5d36f235fc0c9aa81191cc6d16bdc5b3b2aa7
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-11-06 16:32:44 +0900

    learn: Next.js learn Assets, Metadata, and CSS ~ API Routes
    https://nextjs.org/learn/basics/assets-metadata-css

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,19 @@ import Link from 'next/link';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Head from 'next/head';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   const [todos, setTodos] = useState([]);
 
   const fetchTodos = async () => {
@@ -24,6 +35,23 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              {id}
+              <br />
+              <small>
+                <Date dateString={date}></Date>
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
       <section className={utilStyles.headingMd}>
         <p>[Your Self Introduction]</p>
         <p>
@@ -40,6 +68,7 @@ export default function Home() {
           <p key={todo.id}>{todo.title}</p>
         ))}
       </div>
+      <section className={``}></section>
     </Layout>
   );
 }
